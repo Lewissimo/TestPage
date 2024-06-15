703e7f722ddbdaaac3bba4646c5bc134af3b318d
LukaszWitt <witt.config@gmail.com>
2024-06-15 19:05:59 +0200

    alpha 1.0

--- src/components/ExactOffer.jsx
+++ src/components/ExactOffer.jsx
@@ -1,21 +1,64 @@
-import { Box, Card } from "@mui/material";
-import React, { useContext, useEffect } from "react";
-import { ContentContext } from "../context/ContentContext";
+import { Box, Typography, Modal, List, ListItem, ListItemText, ListItemIcon } from "@mui/material";
+import React from "react";
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 
-const ExactOffer = () => {
-  const contentContext = useContext(ContentContext);
-  useEffect(() => {
-    if(contentContext !== undefined){
-        if (contentContext.component !== null) {
-            
-        }
-    }
-  }, [contentContext]);
-
+const ExactOffer = ({ open, handleClose, service }) => {
   return (
-    <Box width={"300px"} height={"400px"}>
-      test
-    </Box>
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="service-modal-title"
+      aria-describedby="service-modal-description"
+    >
+      <Box
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: "90%", // Szerokość 90% dla małych ekranów
+          maxWidth: "1500px", // Maksymalna szerokość dla dużych ekranów
+          maxHeight: "90%", // Maksymalna wysokość
+          height: 'auto', // Automatyczna wysokość
+          bgcolor: "#031B31",
+          color: "#FDD13C",
+          boxShadow: 24,
+          p: 4,
+          display: "flex",
+          flexDirection: "row",
+          outline: "none",
+          overflowY: "auto", // Dodaj przewijanie pionowe, jeśli treść przekracza wysokość
+          "@media (max-width: 600px)": { // Stylizacja dla małych ekranów
+            flexDirection: "column",
+            width: "95%",
+            height: "auto",
+          }
+        }}
+      >
+        <Box sx={{ width: { xs: "100%", sm: "33%" }, paddingRight: { xs: 0, sm: 2 }, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          {service.image ? (
+            <img src={service.image} alt={service.title} style={{ width: "80%", maxHeight: '300px', objectFit: 'cover' }} />
+          ) : (
+            service.icon && React.cloneElement(service.icon, { sx: { fontSize: 100, color: "#FDD13C" } })
+          )}
+        </Box>
+        <Box sx={{ width: { xs: "100%", sm: "67%" }, paddingLeft: { xs: 0, sm: 2 } }}>
+          <Typography id="service-modal-title" variant="h4" component="div" gutterBottom>
+            {service.title}
+          </Typography>
+          <List>
+            {service.description.split('\n').map((item, index) => (
+              <ListItem key={index}>
+                <ListItemIcon>
+                  <FiberManualRecordIcon sx={{ color: "#FDD13C", fontSize: 12 }} /> {/* Ikona kropki */}
+                </ListItemIcon>
+                <ListItemText primary={item} />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Box>
+    </Modal>
   );
 };
 
--- src/layout/Content.jsx
+++ src/layout/Content.jsx
@@ -1,51 +1,89 @@
 import { Box, Grid } from '@mui/material';
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import ContentOption from '../components/ContentOption';
-import img from '../assets/test.png';
+import ExactOffer from '../components/ExactOffer';
 import { ContentContext } from '../context/ContentContext';
 import ElectricalServicesIcon from '@mui/icons-material/ElectricalServices';
-import HomeIcon from '@mui/icons-material/Home'; // Użyjemy Home jako zamiennika dla SmartHome
+import HomeIcon from '@mui/icons-material/Home'; 
 import SpeedIcon from '@mui/icons-material/Speed';
 import VerifiedIcon from '@mui/icons-material/Verified';
 import PoolIcon from '@mui/icons-material/Pool';
 import FoundationIcon from '@mui/icons-material/Foundation';
 
+const serviceData = [
+  {
+    title: 'INSTALACJE ELEKTRYCZNE',
+    description: 'Projektowanie i montaż instalacji elektrycznych w nowych budynkach mieszkalnych i komercyjnych.\nModernizację i rozbudowę istniejących instalacji.\nInstalację i konfigurację systemów oświetleniowych, zarówno wewnętrznych, jak i zewnętrznych.\nMontaż systemów zasilania awaryjnego, w tym generatorów i UPS.\nInstalację systemów zabezpieczeń, takich jak alarmy, monitoring i systemy kontroli dostępu',
+    image: '', // Ikona na karcie
+    modalImage: '/mnt/data/image.png', // Obraz w modalu
+    icon: <ElectricalServicesIcon sx={{ fontSize: 60 }} />,
+  },
+  {
+    title: 'INSTALACJE SMART-HOME',
+    description: 'Zdalne sterowanie oświetleniem, ogrzewaniem, klimatyzacją i innymi urządzeniami.\nIntegracja z systemami alarmowymi i monitoringu dla zwiększenia bezpieczeństwa.\nAutomatyzacja rutynowych czynności, takich jak otwieranie/zamykanie rolet czy podlewanie ogrodu.\nWykorzystanie polskiego producenta AMPIO, co gwarantuje wysoką jakość i niezawodność systemów.\nPersonalizowane rozwiązania dostosowane do potrzeb klienta.',
+    image: '', // Ikona na karcie
+    modalImage: '/mnt/data/image.png', // Obraz w modalu
+    icon: <HomeIcon sx={{ fontSize: 60 }} />,
+  },
+  {
+    title: 'POMIARY ELEKTRYCZNE (UBEZPIECZENIA)',
+    description: 'Przeglądy i pomiary instalacji elektrycznych w budynkach mieszkalnych, komercyjnych i przemysłowych.\nWystawianie raportów i certyfikatów niezbędnych do ubezpieczenia nieruchomości.\nPomiary rezystancji izolacji, pętli zwarcia, wyłączników różnicowoprądowych i inne.\nDoradztwo w zakresie poprawy bezpieczeństwa instalacji elektrycznych.',
+    image: '', // Ikona na karcie
+    modalImage: '', // Brak obrazu, użycie ikony w modalu
+    icon: <SpeedIcon sx={{ fontSize: 60 }} />,
+  },
+  {
+    title: 'WYSTAWIANIE CERTYFIKATÓW ENERGETYCZNYCH',
+    description: 'Ocena efektywności energetycznej budynków.\nWystawianie świadectw charakterystyki energetycznej, które są wymagane przy sprzedaży i wynajmie nieruchomości.\nDoradztwo w zakresie poprawy efektywności energetycznej budynków.\nAnaliza i rekomendacje dotyczące modernizacji instalacji i systemów grzewczych.',
+    image: '', // Ikona na karcie
+    modalImage: '', // Brak obrazu, użycie ikony w modalu
+    icon: <VerifiedIcon sx={{ fontSize: 60 }} />,
+  },
+  {
+    title: 'INSTALACJE BASENOWE',
+    description: 'Projektowanie i montaż systemów oświetlenia basenowego.\nInstalacja systemów filtracji i ogrzewania wody.\nMontaż i konfiguracja systemów zabezpieczeń basenowych, takich jak alarmy i monitoring.\nIntegracja z systemami Smart-Home dla zdalnego sterowania i monitorowania.',
+    image: '', // Ikona na karcie
+    modalImage: '/mnt/data/image.png', // Obraz w modalu
+    icon: <PoolIcon sx={{ fontSize: 60 }} />,
+  },
+  {
+    title: 'REGENERACJA FUNDAMENTÓW',
+    description: 'Diagnostyka i ocena stanu technicznego fundamentów.\nWykonanie prac naprawczych, takich jak iniekcje uszczelniające i wzmacnianie strukturalne.\nZabezpieczenia przed wilgocią i wodą gruntową.\nMonitorowanie i kontrola stanu fundamentów po zakończeniu prac.',
+    image: '', // Ikona na karcie
+    modalImage: '/mnt/data/image.png', // Obraz w modalu
+    icon: <FoundationIcon sx={{ fontSize: 60 }} />,
+  },
+];
+
 const Content = () => {
   const contentContext = useContext(ContentContext);
+  const [open, setOpen] = useState(false);
+  const [selectedService, setSelectedService] = useState(null);
 
-  const generateExactOffer = (e) => {
-    const element = e.currentTarget;
-    const rect = element.getBoundingClientRect();
-    console.log(rect.left);
-    console.log(rect.top);
-    contentContext.setCoordinateX(rect.left);
-    contentContext.setCoordinateY(rect.top);
+  const handleOpen = (service) => {
+    console.log("Opening service:", service);
+    setSelectedService(service);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    console.log("Closing modal");
+    setOpen(false);
+    setSelectedService(null);
   };
 
   return (
-    <Box sx={{
-      backgroundColor: "#031B31"
-    }}>
-      <Grid container justifyContent={'center'}>
-        <Grid display={'flex'} justifyContent={'center'} onClick={generateExactOffer} item xs={12} sm={6} lg={4}>
-          <ContentOption text='KLIKNIJ ABY DOWIEDZIEĆ SIĘ WIĘCEJ' image={<ElectricalServicesIcon sx={{ fontSize: 100 }} />} title={'INSTALACJE ELEKTRYCZNE'} />
-        </Grid>
-        <Grid display={'flex'} justifyContent={'center'} onClick={generateExactOffer} item xs={12} sm={6} lg={4}>
-          <ContentOption text='KLIKNIJ ABY DOWIEDZIEĆ SIĘ WIĘCEJ' image={<HomeIcon sx={{ fontSize: 100 }} />} title={'INSTALACJE SMART-HOME'} />
-        </Grid>
-        <Grid display={'flex'} justifyContent={'center'} onClick={generateExactOffer} item xs={12} sm={6} lg={4}>
-          <ContentOption text='KLIKNIJ ABY DOWIEDZIEĆ SIĘ WIĘCEJ' image={<SpeedIcon sx={{ fontSize: 100 }} />} title={'POMIARY ELEKTRYCZNE (UBEZPIECZENIA)'} />
-        </Grid>
-        <Grid display={'flex'} justifyContent={'center'} onClick={generateExactOffer} item xs={12} sm={6} lg={4}>
-          <ContentOption text='KLIKNIJ ABY DOWIEDZIEĆ SIĘ WIĘCEJ' image={<VerifiedIcon sx={{ fontSize: 100 }} />} title={'WYSTAWIANIE CERTYFIKATÓW ENERGETYCZNYCH'} />
-        </Grid>
-        <Grid display={'flex'} justifyContent={'center'} onClick={generateExactOffer} item xs={12} sm={6} lg={4}>
-          <ContentOption text='KLIKNIJ ABY DOWIEDZIEĆ SIĘ WIĘCEJ' image={<PoolIcon sx={{ fontSize: 100 }} />} title={'INSTALACJE BASENOWE'} />
-        </Grid>
-        <Grid display={'flex'} justifyContent={'center'} onClick={generateExactOffer} item xs={12} sm={6} lg={4}>
-          <ContentOption text='KLIKNIJ ABY DOWIEDZIEĆ SIĘ WIĘCEJ' image={<FoundationIcon sx={{ fontSize: 100 }} />} title={'REGENERACJA FUNDAMENTÓW'} />
-        </Grid>
+    <Box sx={{ backgroundColor: "#031B31", paddingTop: '10px' }}>
+      <Grid container justifyContent={'center'} spacing={2}>
+        {serviceData.map((service, index) => (
+          <Grid key={index} display={'flex'} justifyContent={'center'} onClick={() => handleOpen(service)} item xs={12} sm={6} lg={4}>
+            <ContentOption text='KLIKNIJ ABY DOWIEDZIEĆ SIĘ WIĘCEJ' image={service.icon} title={service.title} />
+          </Grid>
+        ))}
       </Grid>
+      {selectedService && (
+        <ExactOffer open={open} handleClose={handleClose} service={selectedService} />
+      )}
     </Box>
   );
 };
